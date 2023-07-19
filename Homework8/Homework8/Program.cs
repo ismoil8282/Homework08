2c0530e34e677ee26f3985b3020b675a6704b333
ismoil8282 <137428819+ismoil8282@users.noreply.github.com>
2023-07-19 22:23:47 +0300

    task1

---
+++ Homework8/Homework8/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Homework8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the gaps : ");
+            string s = (Console.ReadLine());
+
+           RepeatWord(s);
+            
+Main(args);
+        }
+        
+         public static void RepeatWord(string s) 
+        {
+            int countChar = 1;
+            int counter = 0;
+            char c=' ';
+            for(int i = 0; i < s.Length-1; i++)
+            {
+                if (s[i] == s[i + 1])
+                {
+                    countChar ++;
+                    c = s[i];
+                }else
+                {
+                    countChar=1; 
+                }
+            } 
+            Console.WriteLine(c + " was repeated " + countChar + " times");
+        
+        }
+    }
+}
